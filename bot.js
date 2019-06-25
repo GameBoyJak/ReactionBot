11a95f80b105243149296f092086879ff1a8e8dd
GameBoyJak <parrottoalex@gmail.com>
2019-06-24 23:11:19 -0400

    Finished base functionality

--- bot.js
+++ bot.js
@@ -23,17 +23,111 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
-
+        var random = getRndInteger(0,2);
         args = args.splice(1);
         switch(cmd) {
-            // !ping
-            case 'random':
-                bot.uploadFile({
+            // send random angry reaction image
+            case 'angry':
+              switch(random){
+                case 0:
+                  bot.uploadFile({
                     to: channelID,
-                    message: 'images/negative-cat.jpg'
-                });
+                    file: 'images/angry/negative_cat.jpg'
+                  });
+                  break;
+                case 1:
+                  bot.uploadFile({
+                    to: channelID,
+                    file: 'images/angry/Salt_Nut.png'
+                  });
+              }
+            break;
+
+            case 'angry':
+              switch(random){
+                case 0:
+                  bot.uploadFile({
+                    to: channelID,
+                    file: 'images/angry/negative_cat.jpg'
+                  });
+                  break;
+                case 1:
+                  bot.uploadFile({
+                    to: channelID,
+                    file: 'images/angry/Salt_Nut.png'
+                  });
+              }
+            break;
+
+            case 'confused':
+              switch(random){
+                case 0:
+                  bot.uploadFile({
+                    to: channelID,
+                    file: 'images/confused/Shrek.PNG'
+                  });
+                  break;
+                case 1:
+                  bot.uploadFile({
+                    to: channelID,
+                    file: 'images/confused/Tom.jpg'
+                  });
+              }
+            break;
+
+            case 'excited':
+              switch(random){
+                case 0:
+                  bot.uploadFile({
+                    to: channelID,
+                    file: 'images/excited/Patrick_CloseUp.PNG'
+                  });
+                  break;
+                case 1:
+                  bot.uploadFile({
+                    to: channelID,
+                    file: 'images/excited/super_nut.jpg'
+                  });
+              }
+            break;
+
+            case 'retaliation':
+              switch(random){
+                case 0:
+                  bot.uploadFile({
+                    to: channelID,
+                    file: 'images/retaliation/nae-nae.PNG'
+                  });
+                  break;
+                case 1:
+                  bot.uploadFile({
+                    to: channelID,
+                    file: 'images/retaliation/Peace.JPG'
+                  });
+              }
+            break;
+
+            case 'smug':
+              switch(random){
+                case 0:
+                  bot.uploadFile({
+                    to: channelID,
+                    file: 'images/smug/Smart_Anime.jpg'
+                  });
+                  break;
+                case 1:
+                  bot.uploadFile({
+                    to: channelID,
+                    file: 'images/smug/Surreal_Pepe.PNG'
+                  });
+              }
             break;
             // Just add any case commands if you want to..
          }
      }
 });
+
+// Get random integer between minimum and maximum
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
